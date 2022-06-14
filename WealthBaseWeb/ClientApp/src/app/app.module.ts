57c27536f12dcfa3cd87866fd22d08730c35c420
steve <simmsstev@gmail.com>
2022-06-14 09:27:13 -0700

    installed and styled material nav bar

--- WealthBaseWeb/ClientApp/src/app/app.module.ts
+++ WealthBaseWeb/ClientApp/src/app/app.module.ts
@@ -11,7 +11,15 @@ import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { CryptoTradingComponent } from './crypto-trading/crypto-trading.component';
 import { DynamicCoinComponent } from './dynamic-coin/dynamic-coin.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatNavComponent } from './mat-nav/mat-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
@@ -22,11 +30,13 @@ import { DynamicCoinComponent } from './dynamic-coin/dynamic-coin.component';
     FetchDataComponent,
     CryptoTradingComponent,
     DynamicCoinComponent,
+    MatNavComponent,
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    MatSliderModule,
 
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
@@ -34,7 +44,14 @@ import { DynamicCoinComponent } from './dynamic-coin/dynamic-coin.component';
       {path: 'price', component: DynamicCoinComponent, },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
-    ])
+    ]),
+      BrowserAnimationsModule,
+      LayoutModule,
+      MatToolbarModule,
+      MatButtonModule,
+      MatSidenavModule,
+      MatIconModule,
+      MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ WealthBaseWeb/ClientApp/src/app/mat-nav/mat-nav.component.ts
@@ -0,0 +1,72 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-mat-nav',
+  template: `
+    <mat-sidenav-container class="sidenav-container">
+      <mat-sidenav #drawer class="sidenav" fixedInViewport
+          [attr.role]="(isHandset$ | async) ? 'dialog' : 'navigation'"
+          [mode]="(isHandset$ | async) ? 'over' : 'side'"
+          [opened]="(isHandset$ | async) === false">
+        <mat-toolbar>Menu</mat-toolbar>
+        <mat-nav-list>
+          <a class="nav-link text-dark" [routerLink]="['/']">Home</a>
+          <a class="nav-link text-dark" [routerLink]="['/trade']"
+          >Trading</a
+          >
+          <a mat-list-item href="#">Assets </a>
+        </mat-nav-list>
+      </mat-sidenav>
+      <mat-sidenav-content>
+        <mat-toolbar color="primary">
+          <button
+            type="button"
+            aria-label="Toggle sidenav"
+            mat-icon-button
+            (click)="drawer.toggle()"
+            *ngIf="isHandset$ | async">
+            <mat-icon aria-label="Side nav toggle icon">menu</mat-icon>
+          </button>
+          <span>WealthBaseWeb</span>
+        </mat-toolbar>
+        <!-- Add Content Here -->
+      </mat-sidenav-content>
+    </mat-sidenav-container>
+
+  `,
+  styles: [`
+    .sidenav-container {
+      /*adjust height after installing new card*/
+      height: 10%;
+    }
+
+    .sidenav {
+      width: 200px;
+    }
+
+    .sidenav .mat-toolbar {
+      background: inherit;
+    }
+
+    .mat-toolbar.mat-primary {
+      position: sticky;
+      top: 0;
+      z-index: 1;
+    }
+
+  `]
+})
+export class MatNavComponent {
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+}
