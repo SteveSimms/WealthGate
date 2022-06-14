82db2e1e95cdc396fb5eb7daffc67638bca640be
steve <simmsstev@gmail.com>
2022-06-13 18:27:47 -0700

    Completed Dynamic data transfer ez money

--- WealthBaseWeb/ClientApp/src/app/dynamic-coin/dynamic-coin.component.ts
+++ WealthBaseWeb/ClientApp/src/app/dynamic-coin/dynamic-coin.component.ts
@@ -5,13 +5,24 @@ const log = console.log;
 @Component({
   selector: 'app-dynamic-coin',
   template: `
-    <ul>
 
-      <li *ngFor="let coin of historyState">
-        {{coin}}
-      </li>
-    </ul>
 
+    <div  *ngFor="let coin of historyState" class="row row-cols-1 row-cols-md-2 g-4">
+      <div class="col">
+        <div class="card">
+          <img
+            [src]="coin?.logo_url"
+            style="height: 32px; width: 32px;"
+            class="card-img-top" alt="...">
+          <h4>{{coin?.name}}</h4>
+          <p>{{coin?.symbol}}</p>
+          <div class="card-body">
+            <h5 class="card-title"> {{coin.name}}</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          </div>
+        </div>
+      </div>
+    </div>
   `,
   styles: [
   ]
